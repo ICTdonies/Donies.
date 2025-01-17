be23d80daccb47059833d3773f1db624630c2b4a
ICTdonies <saquineaj@gmail.com>
2025-01-18 02:17:00 +0800

    Add files via upload

---
+++ saquine.h
@@ -0,0 +1,60 @@
+<html>
+
+   <head>
+   <title>HTML IS SO EASY</title>
+   </head>
+         <div style ="text-align:center">
+         <body style ="background-color:grey;">
+         <h1 style="color:green;border:5px solid black;font-family: fantasy ; background-color:#f4b41a"> ALEXIES JOHN SAQUINE</h1>
+              <p>Facebook:<a href="https://www.facebook.com/aj.saquine.3:">aj.saquine.3</a></p>
+              <p>Instragram:<a href="https://www.instagram.com/ajsaquine_0:">ajsaquine_0</a></p>
+                    <div>
+                    <img src=".jpg" width="200" heigth="200"border="3px solid black">
+
+                        <div style="border:5px solid black; background-color:#f4b41a">
+                        <p style="color:darkgreen; border:3px #f4b41a">
+
+                        ABOUT FAMILY<br> 
+
+                        My parents sell barbeque and I'm the second of the siblings. I'm very happy because they've become my family even though there's a problem, they're still happy. and of course the happiest thing in life is bonding because they only have a break once in a while because of selling. and above all we will go to church together.</p>
+
+
+<img src="family.jpg" width="200" height="200"border="3px solid black">
+</div>
+
+                      <div style="border:5px solid black; background-color:#f4b41a">
+                      <p style="color:darkgreen; border:3px #f4b41a;">
+
+                      ABOUT MY HOBBIES, FAVORITE&TALENT<br>
+
+                      my hobbies are watching anime and watching drama and playing online games and drawing. My favorite is menudo, chiken, all pasta. and my talent is dancing and drawing</p>
+
+
+<img src="mydraw.jpg" width="200" height="200">
+</div>
+
+                      <div style="border:5px solid black; background-color:#f4b41a">
+                      <p style="color:darkgreen;  border:3px #f4b41a">
+
+                      ABOUT MY FAVORITE QUETES OR PRINCIPLE<br>
+
+                      My motto in life is will not be defeated by any challenge. and my favorite qoetes success doesn't happen by accident. It is hard work,perseverance,learning,sacrifice,and most important loving what you are doing and learning -pelé.</p>
+
+
+<img src="qoutes.jpg" width="200" height="200">
+</div>
+
+                     <div style="border:5px solid black; background-color:#f4b41a">
+                     <p style ="color:darkgreen;  border:3px #f4b41a">
+
+                     ABOUT MY DREAMS & GOALS<br> 
+
+                     my dream is to be a chef and be an animator. since I was young I wanted to get on a ship to become a chef and I also want to be an animator because I love drawing. and my goals are to graduate from high school and help my families and achieve my dreams.</p>
+
+
+<img src="dream.jpg" width="200" height="200">
+</div>
+
+</body>
+</div>
+</html>
